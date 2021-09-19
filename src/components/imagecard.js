0d09d9954a181db7dbb87e9efa38104394c9658a
Gerardo Torres <68238871+gtor23@users.noreply.github.com>
2021-09-19 02:37:29 -0500

    suppressed the explanation

--- src/components/imagecard.js
+++ src/components/imagecard.js
@@ -10,7 +10,7 @@ const ImageCard = ({image, id}) => {
                 image.media_type == "video" ? (<iframe width="420" height="345" src ={image.url}/>) : <img width="420" height="345" src = {image.url} alt = {'alt'}/>
             }
             <h2>{image.date}</h2>
-            <h3>{image.explanation}</h3>
+            {/* <h3>{image.explanation}</h3> */}
         </div>
     )
 }
